d63ec10312f9dec8a380d4972706cc81d75e793b
ms1969 <maxmaxm@mail.ru>
2021-12-07 21:18:30 +0800

    Dynamic urls

--- horoscope/urls.py
+++ horoscope/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('leo/', views.leo),
-    path('scorpio', views.scorpio),
+    path('<sign_zodiac>/', views.zodiak_info),
 ]
--- horoscope/views.py
+++ horoscope/views.py
@@ -1,11 +1,20 @@
 #from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
-def leo(request):
-    return HttpResponse("Знак зодиака Лев")
 
+def zodiak_info(request, sign_zodiac):
+    if sign_zodiac == 'leo':
+        return HttpResponse("Знак зодиака Лев")
+    elif sign_zodiac == 'scorpio':
+        return HttpResponse("Знак зодиака Скорпион")
+    else:
+        return HttpResponseNotFound(f"Не найден знак {sign_zodiac}")
 
-def scorpio(request):
-    return HttpResponse("Знак зодиака Скорпион")
+# def leo(request):
+#     return HttpResponse("Знак зодиака Лев")
+#
+#
+# def scorpio(request):
+#     return HttpResponse("Знак зодиака Скорпион")
