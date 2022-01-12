249bcfa186f2770783b5b5840e108e07b6e8ab85
ms1969 <maxmaxm@mail.ru>
2022-01-12 21:27:24 +0800

    final horoscope

--- horoscope/urls.py
+++ horoscope/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, converters
 from . import views
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='horoscope-index'),
     path('<int:sign_zodiac>', views.zodiak_info_num),
     path('<str:sign_zodiac>', views.zodiak_info, name='horoscope-name'),
 #    path('<type_zodiac>', views.type_zodiac_info, name='type-name'),
